5111f3913abf7fe71cc697da81b5eeec6b92ee80
Mohammad Mustafa <mustafamohd7636@gmail.com>
2025-11-18 10:54:41 +0530

    commit

---
+++ dfsalgo.py
@@ -0,0 +1,26 @@
+from creategraph import Graph
+
+class GraphAlgos:
+	def __init__(self):
+		pass
+	def depth_first_search(self, graph, vertex):
+		traversed = []
+		stack = []
+		stack.append(vertex)
+		while len(stack) != 0:
+			v = stack.pop()
+			if v not in traversed:
+				traversed.append(v)
+			for element in graph[v]:
+				if element not in traversed:
+					stack.append(element) 
+		for ve in traversed:
+			print (ve, end = ' ')
+		print ()
+obj = Graph(5)
+graph = obj.create_graph()
+print("Graph:", graph)
+
+obj2 = GraphAlgos()
+print ("DFS Traversal order:")
+obj2.depth_first_search(graph, 1)
--- smmr.c
+++ smmr.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
-void SMMR(int i, int j, int a[j+1][j+1], int b[j+1][j+1], int c[j+1][j+1]){
-	if((j-i) ==  0){
-		c[i][j] = a[i][j] * b[i][j];
-	}else{
-		c[i][j] = SMMR(i, j/2, a, b, c) + SMMR((i/2)+1, j, a, b, c);
-		c[i][j+1] = SMMR(i, (j/2)+1, a, b, c) + SMMR((i/2)+1, (j/2)+1, a, b, c);
-		c[i+1][j] = SMMR((i+1)/2, (j/2), a, b, c) + SMMR((i+1)/2, (j/2)+1, a, b, c);
-		c[i+1][j+1] = SMMR((i+1)/2, (j/2)+1, a, b, c) + SMMR((i+1)/2, j, a, b, c);
+#define N 3
+void smmr(int i, int j, int k, int a[N][N], int b[N][N], int c[N][N]){
+	if(i >= N) return;
+	if(j >= N) {
+		smmr(i+1, 0, 0, a, b, c);
+		return;
+	}
+	if(k < N) {
+		c[i][j] += a[i][k] * b[k][j];
+		smmr(i, j, k+1, a, b, c);
+	} else {
+		 smmr(i, j+1, 0, a, b, c);
 	}
 }
 void printArray(int row, int col, int a[row][col]){
@@ -31,10 +35,8 @@ int main(int argc, char** argv){
 
 	printf("Second Matrix: \n");
 	printArray(row, col, sd);
-	
-	int i = 0;
-	int j = 0;
-	SMMR(i, j, td, sd, result);
+
+	smmr(0, 0, 0, td, sd, result);
 
 	printf("Matrix after multiplication: \n");
 	printArray(row, col, result);
